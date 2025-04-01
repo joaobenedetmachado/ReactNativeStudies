c2e78fde96ef5a3e466c5ea1d92a9dc427b1c7e7
joaobenedetmachado <jvbm2177@gmail.com>
2025-04-01 15:09:36 -0300

    verificacao para logat

--- AtividadeSM/screens/Login.jsx
+++ AtividadeSM/screens/Login.jsx
@@ -1,33 +1,50 @@
+import {useState} from 'react';
 import { StyleSheet, Text, View, TextInput, Image, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 export default function Login() {
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
     const navigation = useNavigation();
+
+    const ChangeToPage = () => {
+        if (email && senha) {  
+            navigation.navigate('Page');
+        } else {
+            alert('Por favor, preencha email e senha');
+        }
+    };
+
     return (
-        <View style={style.container}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#252525' }}>
 
-            <Image style={{ width: 150, height: 150, marginBottom: 80 }} source={{ uri: "https://seeklogo.com/images/T/top-fit-academia-logo-2E965040AC-seeklogo.com.png" }} />
+            <Image 
+                style={{ width: 150, height: 150, marginBottom: 80 }} 
+                source={{ uri: "https://seeklogo.com/images/T/top-fit-academia-logo-2E965040AC-seeklogo.com.png" }} 
+            />
 
-            <View style={{width: '100%'}}>
+            <View style={{ width: '80%' }}>
                 <Text style={{ color: '#fff' }}>Email</Text>
-
                 <TextInput
                     placeholder='Seu melhor email'
-                    style={style.input}
+                    style={{ backgroundColor: '#fff', marginBottom: 10, padding: 10, borderRadius: 5 }}
+                    onChangeText={setEmail}
+                    value={email}
                 />
                 <Text style={{ color: '#fff' }}>Senha</Text>
                 <TextInput
                     placeholder='Senha'
-                    style={style.input}
+                    style={{ backgroundColor: '#fff', marginBottom: 10, padding: 10, borderRadius: 5 }}
+                    onChangeText={setSenha}
+                    value={senha}
+                    secureTextEntry
                 />
-                                <Button 
-                    onPress={() => navigation.navigate('Page')} 
-                    title="Entrar" 
-                />
+                <Button title="Entrar" onPress={ChangeToPage} />
             </View>
+
         </View>
-    )
-}
+    );
+};
 
 const style = StyleSheet.create({
     input: {
